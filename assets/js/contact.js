59750abc0aa41b7c837bc8fcd159b6f2f2f3d128
crypticCaroline <rebecca.kelsall@sky.com>
2020-12-30 21:00:14 +0000

    FIXED BUG with the emails

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -3,31 +3,20 @@ let resultStr = " ";
 function sendMail(resultsForm) {
     let wordCount = document.getElementsByClassName('word-count');
     let overlayColour= document.getElementsByClassName("overlay-color");
-    let wordStr = "";
-    let str1 = "";
-    let str2 = ""
-
-
+    
     for(let i = 0; i < wordCount.length; i++) {
-        str1 = wordCount[i].innerHTML;        
-        for(let j = 0; j < overlayColour.length; j++) {
-            str2 = overlayColour[j].innerHTML;   
-            wordStr = ("You read " + str1 + " with " + str2 + "! \n")  
-            resultStr += wordStr               
-        }
-        
-            
-         console.log(resultStr)
+        let str1 = wordCount[i].innerHTML;
+        let str2 = overlayColour[i].innerHTML;
+        let wordStr = ("You read " + str1 + " with " + str2 + "! \n");  
+        resultStr += wordStr;
     }
        
-    // emailjs.send("Project-Request","overlay", {
-    //     "name": resultsForm.name.value,
-    //     "email": resultsForm.email.value,
-    //     "results" : resultStr
-        
-    // })
-    // .then 
-    // alert ("Email Sent")
-    return false;
-    
+    emailjs.send("Project-Request","overlay", {
+        "name": resultsForm.name.value,
+        "email": resultsForm.email.value,
+        "results" : resultStr        
+    })
+    .then 
+    alert ("Email Sent")
+    return false;    
 };
